ca8d76fc68b1c707e85855a8ce8b354c1f484ef6
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-06-21 22:05:15 -0700

    some work on loadLayout()

--- Keyboard Tester_files/ketboardLayout.js
+++ Keyboard Tester_files/ketboardLayout.js
@@ -28,4 +28,17 @@ function loadLayout(unformatedKeyList)
     var allUn = document.getElementsByClassName("key_un");
     var allPressed = document.getElementsByClassName("key_pressed");
     var allLight = document.getElementsByClassName("key_highlight");
+    for(const element of allUn) {
+        element.className = "hidden";
+    }
+    for(const element of allPressed) {
+        element.className = "hidden";
+    }
+    for(const element of allLight) {
+        element.className = "hidden";
+    }
+
+    for(const e of keyList){
+        $(e).removeClass("hidden");
+    }
 }
